10a7a6d2f96d2f30dea7e77848015c47af2e9314
光光 <kevinliu830829@163.com>
2017-09-15 15:02:04 +0800

    增加机房管理模块

---
+++ ComputerRoomManagerment/models.py
@@ -0,0 +1,95 @@
+
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+
+class CompressedTextField(models.TextField):
+    """
+    model Fields for storing text in a compressed format (bz2 by default)
+    """
+
+    def from_db_value(self, value, expression, connection, context):
+        if not value:
+            return value
+        try:
+            return value.decode('base64').decode('bz2').decode('utf-8')
+        except Exception:
+            return value
+
+    def to_python(self, value):
+        if not value:
+            return value
+        try:
+            return value.decode('base64').decode('bz2').decode('utf-8')
+        except Exception:
+            return value
+    def get_prep_value(self, value):
+        if not value:
+            return value
+        try:
+            value.decode('base64')
+            return value
+        except Exception:
+            try:
+                return value.encode('utf-8').encode('bz2').encode('base64')
+            except Exception:
+                return value
+
+
+
+#机房管理表
+class ComputerRoomTable(models.Model):
+
+    pub_date = models.DateTimeField(u'发表时间', auto_now_add=True, editable=True)
+    update_time = models.DateTimeField(u'更新时间', auto_now=True, null=True)
+
+    class Meta:
+        verbose_name = '机房信息'
+        verbose_name_plural = '机房信息'
+        ordering = ['']
+
+    def __unicode__(self):
+        return
+
+#服务器信息表
+class ServerMachineTable():
+
+    pub_date = models.DateTimeField(u'发表时间', auto_now_add=True, editable=True)
+    update_time = models.DateTimeField(u'更新时间', auto_now=True, null=True)
+
+    class Meta:
+        verbose_name = '机房信息'
+        verbose_name_plural = '机房信息'
+        ordering = ['']
+
+    def __unicode__(self):
+        return
+
+#网络设备信息表
+class NetworkMachineTable(models.Model):
+
+    pub_date = models.DateTimeField(u'发表时间', auto_now_add=True, editable=True)
+    update_time = models.DateTimeField(u'更新时间', auto_now=True, null=True)
+
+    class Meta:
+        verbose_name = '机房信息'
+        verbose_name_plural = '机房信息'
+        ordering = ['']
+
+    def __unicode__(self):
+        return
+
+#机柜信息表
+class CabinetTable(models.Model):
+
+    pub_date = models.DateTimeField(u'发表时间', auto_now_add=True, editable=True)
+    update_time = models.DateTimeField(u'更新时间', auto_now=True, null=True)
+
+    class Meta:
+        verbose_name = '机房信息'
+        verbose_name_plural = '机房信息'
+        ordering = ['']
+
+    def __unicode__(self):
+        return
